49f2a434b8bea6456eb2a18f9c34142a7a00a0c9
Julie Lee <julielee@Julies-MacBook-Air.local>
2024-12-17 12:48:25 -0600

    cleaning up lists and reviews

--- app/models/list.rb
+++ app/models/list.rb
@@ -8,6 +8,7 @@ class List < ApplicationRecord
 
   validates :status, presence: true
   validates :name, uniqueness: { scope: [ :user_id, :status ], message: "should be unique per user for default lists" }, if: -> { default? }
+  validates :name, presence: true
 
   # defining scope for easier use later
   scope :default_lists, -> { where(status: :default) }
